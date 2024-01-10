a9c1ce9dfbba4cd8c874b9e8726572ddea0d6511
khovorsuvd <90038860+khovorsuvd@users.noreply.github.com>
2024-01-10 06:37:57 +0300

    fix

--- config.php
+++ config.php
@@ -1,13 +1,10 @@
 <?php 
-/* Stripe API configuration 
- * Remember to switch to your live publishable and secret key in production! 
- * See your keys here: https://dashboard.stripe.com/account/apikeys 
- */ 
+
 define('STRIPE_API_KEY', 'sk_test_51OUqNlDxIChVdQml1po5HPkMtyQaJdlwCHg3UBUr5Hf2sF48WCsjieeXTW7OpJuJYZUzjNwIBSnS7VcTQ6dWxQ3v00xDpjPVPx'); 
 define('STRIPE_PUBLISHABLE_KEY', 'pk_test_51OUqNlDxIChVdQmlAVcetNtPnloBfFvEekPrFcoN3dP8VwScNOkAlnuZHhtDE8AKlGyBpsjAGRL5KgXuUUDpucTy005ek4Xyzt'); 
 define('STRIPE_CURRENCY', 'USD'); 
   
-// Database configuration  
+  
 define('DB_HOST', 'localhost:3307'); 
 define('DB_USERNAME', 'root'); 
 define('DB_PASSWORD', '1111'); 
